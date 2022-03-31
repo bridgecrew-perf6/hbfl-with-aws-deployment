abf4075cb850f146d93252f307533d9b0a21a7ed
INFOR\vvillaruel <vonivan.villaruel@infor.com>
2022-03-31 21:32:06 +0800

    Enable cloud front distribution

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://hamster-bucket-von-2022.s3.amazonaws.com'
+const base = 'https://d3rv0oph1y9u45.cloudfront.net'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
